c8257a2e2073dad6e32f18c6d5e985ce8e5970b4
CongDucc <duc.tc18948@sinhvien.hoasen.edu.vn>
2025-06-09 19:32:02 +0700

    initial commit with prime and test

---
+++ unitTest.py
@@ -0,0 +1,30 @@
+import unittest
+from prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+
+    def test_negative(self):
+        self.assertFalse(is_prime(-1))
+        self.assertFalse(is_prime(0))
+        self.assertFalse(is_prime(1))
+
+    def test_small_primes(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime(7))
+
+    def test_small_non_primes(self):
+        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(6))
+        self.assertFalse(is_prime(9))
+        self.assertFalse(is_prime(10))
+
+    def test_large_prime(self):
+        self.assertTrue(is_prime(97))
+
+    def test_large_non_prime(self):
+        self.assertFalse(is_prime(100))
+
+if __name__ == '__main__':
+    unittest.main()
