243f2d0964aed0e03ee31c5bde31018fba008dd4
CongDucc <duc.tc18948@sinhvien.hoasen.edu.vn>
2025-06-09 19:42:23 +0700

    Add failer

--- unitTest.py
+++ unitTest.py
@@ -12,20 +12,13 @@ class TestIsPrime(unittest.TestCase):
         self.assertTrue(is_prime(2))
         self.assertTrue(is_prime(3))
         self.assertTrue(is_prime(5))
-        self.assertTrue(is_prime(7))
-        self.assertTrue(is_prime(9))  # <--- Đây là số không phải số nguyên tố!
+        self.assertTrue(is_prime(9))  # ❌ 9 không phải số nguyên tố!
 
     def test_small_non_primes(self):
         self.assertFalse(is_prime(4))
         self.assertFalse(is_prime(6))
-        self.assertFalse(is_prime(9))
+        self.assertFalse(is_prime(8))
         self.assertFalse(is_prime(10))
-
-    def test_large_prime(self):
-        self.assertTrue(is_prime(97))
-
-    def test_large_non_prime(self):
-        self.assertFalse(is_prime(100))
 
 if __name__ == '__main__':
     unittest.main()
